85333a40e691b98c9b0028f059bc629059fc5e2a
lazycoder2021 <tksingh2087@gmail.com>
2021-02-14 09:09:16 +0530

    first committ

---
+++ src/components/elements/Spinner/Spinner.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loader">
+            
+        </div>
+
+    )
+
+}
+
+export default Spinner;
+
+
+
+
+
+
+
+
+
+// JavaScript source code
+// JavaScript source code
+// JavaScript source code
+
+
+
+
+
+
+
+
+
+// JavaScript source code
