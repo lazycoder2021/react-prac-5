a9e39511bd68d907c7d8891d63253bb98c476c74
lazycoder2021 <tksingh2087@gmail.com>
2021-02-14 09:17:47 +0530

    Updates

---
+++ precache-manifest.e95e91aa5549830f3dc93a88662b4b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06505294271be530061b3bb62c743f57",
+    "url": "/react-prac-5/index.html"
+  },
+  {
+    "revision": "023e08960ab1af439009",
+    "url": "/react-prac-5/static/css/main.675582e4.chunk.css"
+  },
+  {
+    "revision": "42849f3f8cad04b22f09",
+    "url": "/react-prac-5/static/js/2.dd22d9c5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-prac-5/static/js/2.dd22d9c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "023e08960ab1af439009",
+    "url": "/react-prac-5/static/js/main.36cb1bef.chunk.js"
+  },
+  {
+    "revision": "c8876aa450ee4b961278",
+    "url": "/react-prac-5/static/js/runtime-main.68511d28.js"
+  }
+]);
